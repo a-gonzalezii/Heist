43b915d4845ee8ebbe9fc150604249278b09c1b7
Arturo <a.gonzalezii609@gmail.com>
2014-03-17 17:34:30 -0400

    basic assets and movement.

---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+
+var speed : float = 10;
+var jumpSpeed : float = 10;
+var acceleration: float = 2;
+
+var moveLeft : KeyCode;
+var moveRight : KeyCode;
+var jump : KeyCode;
+var action : KeyCode;
+
+
+function Update () {
+
+	if(Input.GetKey(moveLeft)){
+		rigidbody2D.velocity.x = -speed;
+		
+	}else if(Input.GetKey(moveRight)){
+		rigidbody2D.velocity.x = speed;		
+		
+	}else{
+		rigidbody2D.velocity.x = 0;
+		rigidbody2D.velocity.y = 0;
+	}
+	
+	if(Input.GetKey(action)){
+		//TODO: Interactable Object feature
+		//need to look at collision detection; most likely this will make a call to the InteractableObject that it is colliding with.
+		//on the InteractableObject side there will need to be a boolean that tracks if it is currently colliding with the player. 
+		
+	}if(Input.GetKey(jump)){
+		rigidbody2D.velocity.y = jumpSpeed;
+	
+	}
+
+
+}
