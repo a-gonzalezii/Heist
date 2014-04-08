8b0559aa77dc597cd7773d008a4137c6d0f43aed
Arturo <a.gonzalezii609@gmail.com>
2014-04-08 10:43:43 -0400

    Auto-adjusting platforms and start of lvl 1

--- Assets/Scripts/GameManager.js
+++ Assets/Scripts/GameManager.js
@@ -4,15 +4,23 @@ var player1: Transform;
 var player2: Transform;
 
 var mainCamera : Camera;
-var cameraOffset : Vector3 = new Vector3(0,5,-10);
+var cameraOffset : Vector3 = new Vector3(0,0,-10);
 
-function Start () {
+var platform0: Transform;
+var platform1: Transform;
+var platform2: Transform;
 
-	player1 = player1;
-	player2 = player2;
+function Start () {//TODO: AUTOCONFIG PLATFORM POSITIONS
+	platform0.position = new Vector3 (0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height,0f)).y ,0f);
+	platform1.position = new Vector3 (0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height/2,0f)).y+2,0f);
+	platform2.position = new Vector3 (0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y+2,0f);
+	
+	player1.position = new Vector3(0f, platform1.position.y+1, 0f);
+	player2.position = new Vector3(0f, platform2.position.y+1, 0f);
+	
 }
 
-function Update () {//TODO: SETP BOUNDS ON CAMERA
+function Update () {//TODO: SETUP BOUNDS ON CAMERA
 
 	var xPos = Mathf.Abs(player1.position.x-player2.position.x)/2 + Mathf.Min(player1.position.x,player2.position.x); 
 	mainCamera.transform.position = new Vector3(xPos, 5 , 0) + cameraOffset;
--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -9,7 +9,6 @@ var jumpIsLocked:boolean = false;
 var moveLeft : KeyCode;
 var moveRight : KeyCode;
 var jump : KeyCode;
-var action : KeyCode;
 
 function jumpToUnlock(){
 	jumpIsLocked = false;
@@ -26,13 +25,7 @@ function FixedUpdate () {
 	}else{
 		rigidbody2D.velocity.x = 0;
 	}
-	
-	if(Input.GetKey(action)){
-		//TODO: Interactable Object feature
-		//need to look at collision detection; most likely this will make a call to the InteractableObject that it is colliding with.
-		//on the InteractableObject side there will need to be a boolean that tracks if it is currently colliding with the player. 
-		
-	}if(Input.GetKey(jump)){
+	if(Input.GetKey(jump)){
 		if(!jumpIsLocked){//TODO: NEED TO CHANGE THIS FOR WHEN THEY ARE IN A SPACE THAT ONLY FITS THE USER/player icon
 			rigidbody2D.AddForce(Vector3.up*jumpSpeed);//rigidbody2D.velocity.y = jumpSpeed;
 			jumpIsLocked = true;
