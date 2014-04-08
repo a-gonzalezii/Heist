576fad1a51c3a096730d06579b9cb07045d281c4
Arturo <a.gonzalezii609@gmail.com>
2014-04-08 12:36:06 -0400

    have a platform moving up and down with button.
    Also added some more to the start state of the level.
    There is the issue of the player falling weirdly when on the platform. I
    will be fixing that today.

--- Assets/Scripts/GameManager.js
+++ Assets/Scripts/GameManager.js
@@ -6,17 +6,19 @@ var player2: Transform;
 var mainCamera : Camera;
 var cameraOffset : Vector3 = new Vector3(0,0,-10);
 
-var platform0: Transform;
-var platform1: Transform;
-var platform2: Transform;
+var platform0: GameObject;
+var platform1: GameObject;
+var platform2: GameObject;
 
-function Start () {//TODO: AUTOCONFIG PLATFORM POSITIONS
-	platform0.position = new Vector3 (0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height,0f)).y ,0f);
-	platform1.position = new Vector3 (0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height/2,0f)).y+2,0f);
-	platform2.position = new Vector3 (0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y+2,0f);
-	
-	player1.position = new Vector3(0f, platform1.position.y+1, 0f);
-	player2.position = new Vector3(0f, platform2.position.y+1, 0f);
+function Start () {
+	//Set platforms so that their start position(far left side) is at 0f and they are evenly spaced through the center of the screen. 
+	platform0.transform.position = new Vector3 (platform1.renderer.bounds.size.x/2, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height,0f)).y ,0f);
+	platform1.transform.position = new Vector3 (platform1.renderer.bounds.size.x/2, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height/2,0f)).y+2,0f);
+	platform2.transform.position = new Vector3 (platform2.renderer.bounds.size.x/2, mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y+2,0f);
+
+	//Set player position so they are just above platforms 1 and 2 respectively; could further enhance this by making them at y position = platformPosition+platformHeight+playerHeight
+	player1.position = new Vector3(platform1.transform.position.x - platform1.renderer.bounds.size.x/2+2, platform1.transform.position.y+1, 0f);
+	player2.position = new Vector3(platform2.transform.position.x - platform2.renderer.bounds.size.x/2+2, platform2.transform.position.y+1, 0f);
 	
 }
 
--- Assets/Scripts/InteractibleScript.js
+++ Assets/Scripts/InteractibleScript.js
@@ -1,27 +1,43 @@
 ﻿#pragma strict
 
-var changedObject:Light;
+var changedObject:GameObject;
+var startPosition: Vector3;
+var moving: boolean = false;
 
-function Start(){
-
-	changedObject = changedObject;
-	//changedObject.intensity=0;
-
+function Start(){//TODO: FINE TUNE THIS ISH
+	startPosition = changedObject.transform.position;
+	
 }
 
 function OnTriggerEnter2D(hitInfo : Collider2D){
-	
+
 	if(hitInfo.collider2D.tag == "Player"){
-		changedObject.intensity = 8f;
-		
+		moving = true;
 	}
 	
+}
+
+function OnTriggerStay2D(hitInfo: Collider2D){
+
+	if(hitInfo.collider2D.tag =="Player"){
+		changedObject.transform.position.y += .05;
+	}
 }
 
 function OnTriggerExit2D(hitInfo : Collider2D){
 
 	if(hitInfo.collider2D.tag == "Player"){
-		changedObject.intensity=0f;
+		moving = false;
+	}
+}
+
+function FixedUpdate(){
+
+	if(!moving && Vector3.Distance(startPosition, changedObject.transform.position) > .05){
+		changedObject.transform.position.y -= .05;
 	}
 
+
 }
+
+
