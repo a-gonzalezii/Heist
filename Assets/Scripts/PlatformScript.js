e26227539eccf31256d83b56aee93df276399a21
Arturo <a.gonzalezii609@gmail.com>
2014-04-14 23:02:09 -0400

    minor edits

--- Assets/Scripts/PlatformScript.js
+++ Assets/Scripts/PlatformScript.js
@@ -5,14 +5,13 @@
 function OnCollisionEnter2D (hitInfo : Collision2D){
 	//When landing from a jump, let the player jump again
 	print("collision");
+	
 	if(hitInfo.collider.tag == "Player"){
-		
-		var cp:ContactPoint2D[]=hitInfo.contacts;
+		var cp:ContactPoint2D[] = hitInfo.contacts;
 		var center_y:float = hitInfo.gameObject.renderer.bounds.center.y;
 		
 		for(var i=0;i < cp.length;i++){//only if player is on top of a platform can they jump again
 			if(cp[i].point.y > center_y){
-				print("Player is not on top");
 				return;
 			}
 		}
