dcab7e961eca6fb2d80d08d2acbc50aec5711782
John Kniespeck <john.kniespeck@gmail.com>
2014-04-17 11:20:58 -0400

    Revert 394f344..3011f6b
    This rolls back to commit 394f344f4ff13f50e4f2c31c7e8a38750c92e99a.

--- Assets/Scripts/InteractibleScript.js
+++ Assets/Scripts/InteractibleScript.js
@@ -2,12 +2,9 @@
 
 //set in the scene
 var changedObject:GameObject;//a platform or wall - //TODO:may be able to use just the transform
+var distanceFromTop:float=3f;
 var moving: boolean = false;
-var speed: float = 3;
-var yDist: float = 0;
-var xDist: float = 0;
-
-
+var speed: float=3;
 var mainCamera:Camera;
 
 //set in start()
@@ -16,33 +13,25 @@ var step: float;
 var startPosition: Vector3;
 
 
-function Start(){
-
-	mainCamera=Camera.main;
-
+function Start(){//TODO: FINE TUNE THE SPEEDS
 	startPosition = changedObject.transform.position;
 	step = speed*Time.deltaTime;
-	//Relative to current position, where would you like to move to
-	targetPosition = startPosition + new Vector3(xDist, yDist, 0);
+	//top of screen - 1/2 object height - distance from top.
+	targetPosition = new Vector3(startPosition.x,
+		mainCamera.ScreenToWorldPoint(new Vector3(0f,Screen.height,0f)).y-changedObject.renderer.bounds.extents.y-distanceFromTop,0f);
 }
 //when touch button - the wall is now moving
 function OnTriggerEnter2D(hitInfo : Collider2D){
-	print("here");
 	if(hitInfo.collider2D.tag == "Player"){
 		moving = true;
 	}	
 }
-
-/* Phased out - now everything moves on fixedupdate
 //as long as youre on the button move the wall
-function OnTriggerStay2D(hitInfo: Collider2D){
+function OnTriggerStay2D(hitInfo: Collider2D){//TODO: CANNOT SET POSITION MUST SET VELOCITY
 	if(hitInfo.collider2D.tag =="Player"){
-		var positionPrime:Vector3 = Vector3.MoveTowards(changedObject.transform.position, targetPosition, step);
-		changedObject.transform.Translate(positionPrime - changedObject.transform.position);
-//		changedObject.transform.position = Vector3.MoveTowards(changedObject.transform.position, targetPosition, step);
+		changedObject.transform.position = Vector3.MoveTowards(changedObject.transform.position, targetPosition, step);
 	}
-}*/
-
+}
 //when off button no longer moving wall/moves back to position
 function OnTriggerExit2D(hitInfo : Collider2D){
 	if(hitInfo.collider2D.tag == "Player"){
@@ -52,16 +41,11 @@ function OnTriggerExit2D(hitInfo : Collider2D){
 
 function FixedUpdate(){
 //if the player isnt on the button and the wall hasnt returned to its original position - move toward original position
-	var positionPrime:Vector3;
-
-	if(moving){
-		positionPrime = Vector3.MoveTowards(changedObject.transform.position, targetPosition, step);
-		changedObject.transform.Translate(positionPrime - changedObject.transform.position);
-	}else{
-		if(Vector3.Distance(startPosition, changedObject.transform.position) > .05){
-			positionPrime = Vector3.MoveTowards(changedObject.transform.position, startPosition, step);
-			changedObject.transform.Translate(positionPrime - changedObject.transform.position);
-		}
+	if(!moving && Vector3.Distance(startPosition, changedObject.transform.position) > .05){
+		changedObject.transform.position = Vector3.MoveTowards(changedObject.transform.position, startPosition, step);
 	}
 
+
 }
+
+
